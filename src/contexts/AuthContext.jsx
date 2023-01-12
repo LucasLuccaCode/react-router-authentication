5a5a51657a44fc42a4c6eeb7456dd68c87c769ac
Lucca Santos <lucasluccad.r@outlook.com>
2023-01-12 20:56:15 -0300

    fix: fixed error of not persisting user data when reloading browser

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { requestSignIn } from "../services/auth";
 import Cookies from "js-cookie";
 
@@ -6,6 +6,18 @@ const AuthContext = createContext()
 
 export function AuthProvider({ children }) {
   const [user, setUser] = useState(null)
+
+  useEffect(()=> {
+    const loadDataCache = () => {
+      const token = Cookies.get("router-auth-token")
+      const user = Cookies.get("router-auth-user")
+
+      if(token !== 'undefined' && user !== 'undefined'){
+        setUser(JSON.parse(user))
+      }
+    }
+    loadDataCache()
+  }, [])
 
   const isAuthenticated = !!user
 
